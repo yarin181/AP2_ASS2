d020247af330176b515b9674c21d8ad3c8d9873c
yarin181 <yarinsason@gmail.com>
2023-05-24 11:49:13 +0300

    edit the time stamp

--- src/chat_components/Message.js
+++ src/chat_components/Message.js
@@ -1,10 +1,14 @@
 
 function Message({msg,index,sender}) {
     const className =  sender ? "message sent" : "message received";
+    const date = new Date(msg.created);
+    const hours = date.getHours().toString().padStart(2, '0');
+    const minutes = date.getMinutes().toString().padStart(2, '0');
+    const time =  `${hours}:${minutes}`;
     return (
         <div className={className} id={index}>
             <p className="message-text">{msg.content}</p>
-            <p className="time-stamp">{msg.created}</p>
+            <p className="time-stamp">{time}</p>
         </div>
     );
 }
